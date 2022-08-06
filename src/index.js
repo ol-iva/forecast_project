1883637ae8a22df9427a0cbe3eece807da293bea
ol-iva <lena.ivakh@gmail.com>
2022-08-06 20:32:45 +0200

    Upgrade protocol to icon

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ function showWeather(response) {
 
   iconElement.setAttribute(
       "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
