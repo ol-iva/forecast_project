4510cb6d19c735784ee56ff781d9876c640edcb4
ol-iva <lena.ivakh@gmail.com>
2022-08-06 21:02:43 +0200

    Add unit conversion and weather description

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-let date = document.querySelector("h6.date");
-
+let date = document.querySelector("#date");
 let unit = "metric";
 let searchForm = document.querySelector("#search-form");
 let celsius = null;
+let celsiusLinkElement = document.querySelector("#celsius");
+let fahrenheitLinkElement = document.querySelector("#fahrenheit");
 
 date.innerHTML = formatDate(new Date());
 search('Ljubljana');
 
 searchForm.addEventListener("submit", changeTempByCity);
+fahrenheitLinkElement.addEventListener("click", showTemperatureFarenheit);
+celsiusLinkElement.addEventListener("click", showTemperatureCelsius);
 
 function changeTempByCity(event) {
   event.preventDefault();
@@ -29,12 +32,14 @@ function showWeather(response) {
   let windElement = document.querySelector("span#wind-now");
   let iconElement = document.querySelector("#main-icon");
   let dateElement = document.querySelector("#date");
+  let descriptionElement = document.querySelector("#description");
 
   celsius = weather.main.temp;
   tempElement.innerHTML = Math.round(celsius);
   humidityElement.innerHTML = `${weather.main.humidity} %`;
   windElement.innerHTML = `${weather.wind.speed} km/h`;
-  date.innerHTML = formatDate(new Date(response.data.dt * 1000));
+  dateElement.innerHTML = formatDate(new Date(response.data.dt * 1000));
+  descriptionElement.innerHTML = response.data.weather[0].description;
 
   iconElement.setAttribute(
       "src",
@@ -73,6 +78,24 @@ function formatDate(date) {
   }
 
   return `${weekDay} ${hours}:${minutes}`;
+}
+
+
+function showTemperatureFarenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature-now");
+  let fahrenheitTemperature = (celsius * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  fahrenheitLinkElement.classList.add("active");
+  celsiusLinkElement.classList.remove("active");
+}
+
+function showTemperatureCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature-now");
+  temperatureElement.innerHTML = Math.round(celsius);
+fahrenheitLinkElement.classList.remove("active");
+celsiusLinkElement.classList.add("active");
 }
 
 
